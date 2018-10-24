c2d64a88a9ccca069cf022e52834f24c065eb816
Zach Nelson <zachn@knowledgeservices.com>
2018-10-24 11:48:16 -0400

    created mock DevTeam service

---
+++ KomodoDevTeams.Contracts/IDevTeamService.cs
@@ -0,0 +1,18 @@
+﻿using KomodoDevTeams.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoDevTeams.Contracts
+{
+	public interface IDevTeamService
+	{
+		bool CreateDevTeam(DevTeamCreate model);
+		IEnumerable<DevTeamListItem> GetDevTeams();
+		DevTeamDetails GetDevTeamById(int id);
+		bool UpdateDevTeam(DevTeamEdit model);
+		bool DeleteDevTeam(int id);
+	}
+}
---
+++ KomodoDevTeams.Services/MockServices/MockDevTeamService.cs
@@ -0,0 +1,50 @@
+﻿using KomodoDevTeams.Contracts;
+using KomodoDevTeams.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoDevTeams.Services.MockServices
+{
+	public class MockDevTeamService : IDevTeamService
+	{
+		public bool ReturnValue { get; set; }
+		public int CallCount { get; set; }
+
+		public bool CreateDevTeam(DevTeamCreate model)
+		{
+			CallCount++;
+			return ReturnValue;
+		}
+
+		public bool DeleteDevTeam(int id)
+		{
+			CallCount++;
+			return ReturnValue;
+		}
+
+		public DevTeamDetails GetDevTeamById(int id)
+		{
+			CallCount++;
+			return new DevTeamDetails { TeamId = 1, TeamName = "XML Team" };
+		}
+
+		public IEnumerable<DevTeamListItem> GetDevTeams()
+		{
+			CallCount++;
+			var result = new List<DevTeamListItem>
+			{
+				new DevTeamListItem{TeamId = 1, TeamName = "C# Team"}
+			};
+			return result;
+		}
+
+		public bool UpdateDevTeam(DevTeamEdit model)
+		{
+			CallCount++;
+			return ReturnValue;
+		}
+	}
+}
